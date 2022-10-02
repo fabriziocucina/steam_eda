e02f6c6dbd909ef266eee83a0f5ea14841d24784
Fabrizio Cucina <fabriziocucina23@gmail.com>
2022-10-02 12:09:50 -0500

    version 1.0 finalized

--- etl/transform.py
+++ etl/transform.py
@@ -1,3 +1,4 @@
+from distutils.command.config import config
 import logging
 import pandas as pd
 import numpy as np 
@@ -12,16 +13,28 @@ def clean_column_names(df):
     for column in df.columns: 
         for change in changes:
                 new_column = column.replace(change,"").replace(" ","_")
-        new_columns.append(new_column.capitalize())
+        new_columns.append(new_column.capitalize().strip())
     df.columns = new_columns
     return df
 
 # Clean the rows
 def clean_rows(df):
     logging.info("Normalizing rows...")
-    changes = ["\[", "\]", "\'", "%", "\$", "/app/",",","-","."]
-    for change in changes:
-        df= df.replace(change, "", regex = True)
-    df = df.reset_index(drop = True, inplace = True)
+    changes_in_rows = ["\[", "\]", "\'", "%", "\$", "/app/"]
+    for change in changes_in_rows:
+        df.replace(change, "", regex = True, inplace = True)
+        df.reset_index(drop = True, inplace = True)
     return df
+
+# Trim all values in the rows
+def trim_all_columns(df):
+    logging.info("Triming values...")
+    trim_strings = lambda x: x.strip() if isinstance(x, str) else x
+    df.applymap(trim_strings)
+    return df
+
+# Get results in a excel file
+def save_data(df,path: str):
+    logging.info("Saving results in the folder...")
+    df.to_excel(path["path_extract"]+"\\"+"steam_games_"+str(path["year"])+".xlsx")
 
--- main.py
+++ main.py
@@ -13,16 +13,26 @@ from tqdm import tqdm
 # Import internal libraries
 from etl import scraping
 from configuration import configuration
+from etl import transform
 
 
 def main():
-    # Configuration variables
     logging.info("Starting the scraper...")
     conf = configuration()
     main_url = scraping.get_soup(conf["sites"]["steam_spy_year"]+str(conf["year"]))
     links = scraping.get_games_links(main_url,conf["sites"]["steam_spy_main"])
+    time.sleep(4)
     game_info = scraping.get_game_info(links)
-    scraping.get_excel(game_info,conf["year"])
+    time.sleep(4)
+    data = scraping.get_df(game_info)
+    time.sleep(4)
+    columns = transform.clean_column_names(data)
+    time.sleep(4)
+    rows = transform.clean_rows(columns)
+    time.sleep(4)
+    spaces = transform.trim_all_columns(rows)
+    time.sleep(4)
+    transform.save_data(spaces,conf)
 
 
 if __name__ == "__main__":
